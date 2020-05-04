a010f844c0984931c6808834cdb7031dfc3810dc
michal-char <63314235+michal-char@users.noreply.github.com>
2020-05-05 01:04:12 +0200

    Update za_mało-za_dużo.cpp

--- 28.04.2020/za_mało-za_dużo.cpp
+++ 28.04.2020/za_mało-za_dużo.cpp
@@ -7,21 +7,9 @@ int main()
 {
         //los
         srand(time(0));
-        ios::sync_with_stdio(false);
-        cin.tie(NULL);
         int a = rand() % 1001;
 
-        //napisy
-
-        cout << "                            _            __               _        _       " << endl;
-        cout << "                           | |/         / /              | |      |_|        " << endl;
-        cout << " ______ _   _ __ ___   __ _| / ___     / / ______ _    __| |_   _ _______  " << endl;
-        cout << "|_  / _` | | '_ ` _ \\ / _` |/|/ _ \\   / / /_  / _` |  / _` | | | |_  / _ \\ " << endl;
-        cout << " / / (_| | | | | | | | (_| / | (_) | / /   / / (_| | | (_| | |_| |/ / (_) |" << endl;
-        cout << "/___\\__,_| |_| |_| |_|\\__,/|_|\\___/ /_/   /___\\__,_|  \\__,_|\\__,_/___\\___/ " << endl;
-        cout << "                                                                       " << endl;
-        cout << "                                                                       " << endl;
-        cout << "PODAJ LICZBE Z ZAKRESU <0,1000>" << endl;
+        cout << "PODAJ LICZBE W ZAKRESIE <0,1000>" << endl;
 
         //gra
         int n;
